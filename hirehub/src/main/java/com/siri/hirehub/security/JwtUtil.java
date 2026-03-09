a48104b771620498472f8121444eca0077f120c0
Siri Mohan <smohan2@opentext.com>
2026-03-09 20:55:48 +0530

    Hirehub project

---
+++ hirehub/src/main/java/com/siri/hirehub/security/JwtUtil.java
@@ -0,0 +1,39 @@
+package com.siri.hirehub.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import java.util.Date;
+@Component
+public class JwtUtil {
+
+    private static final String SECRET = "mysupersecretkeymysupersecretkey";
+
+    private static Key getSigningKey() {
+        return new SecretKeySpec(SECRET.getBytes(), SignatureAlgorithm.HS256.getJcaName());
+    }
+
+    public static String generateToken(String email) {
+
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hrs
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public static String extractEmail(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+}
